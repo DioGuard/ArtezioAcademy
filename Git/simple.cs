a002b7131e1431b961825d74d34bf47ba40a31b3
Daniil Gorbunov <dan.gorbunov.00@yandex.ru>
2019-06-05 19:26:00 +0300

    Master#3: edit <simple.cs>, 1 new line added

--- Git/simple.cs
+++ Git/simple.cs
@@ -4,7 +4,10 @@ namespace Name {
     class Program {
         static void Main(string[] arg) {
             Console.WriteLine($"Hello, {arg[0]}");
+            Console.WriteLine($"Who touch my file?");
+
             Console.ReadKey();
+
         }
     }
 }
