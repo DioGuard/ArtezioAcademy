59b2cd19bd530c3e4529cbffcd40e22c51b57874
Daniil Gorbunov <dan.gorbunov.00@yandex.ru>
2019-06-05 19:13:02 +0300

    Master#2:  edit <simple.cs>, fixed bugs

--- Git/simple.cs
+++ Git/simple.cs
@@ -3,8 +3,8 @@ using System;
 namespace Name {
     class Program {
         static void Main(string[] arg) {
-            Console.WriteLine($"Hello, {arg[1]}");
-            Console.Readkey();
+            Console.WriteLine($"Hello, {arg[0]}");
+            Console.ReadKey();
         }
     }
 }
